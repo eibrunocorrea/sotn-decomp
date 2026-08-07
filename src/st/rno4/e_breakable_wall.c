652c79d11f997ac3726912559005c5261e1ed413
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-07 14:40:55 -0300

    rno4: decompile EntityBreakableWallDebris

--- src/st/rno4/e_breakable_wall.c
+++
@@ -1,4 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-#include "rno4.h"
-
-INCLUDE_ASM("st/rno4/nonmatchings/e_breakable_wall", EntityBreakableWallDebris);
--- src/st/rno4/e_secrets.c
+++ src/st/rno4/e_secrets.c
@@ -40,6 +40,7 @@ static s16 breakable_wall_tile_layout[] = {
 // clang-format on
 
 extern EInit g_EInitInteractable;
+extern EInit g_EInitParticle;
 
 void EntityBreakableCrystalFloor(Entity* self) {
     Entity* newEntity;
@@ -195,6 +196,55 @@ void EntityBreakableWall(Entity* self) {
     case 3:
         if (!--self->ext.breakable.resetTimer) {
             self->step = 1;
+        }
+        break;
+    }
+}
+
+void EntityBreakableWallDebris(Entity* self) {
+    Collider collider;
+    Entity* newEntity;
+    s32 posX;
+    s32 posY;
+
+    switch (self->step) {
+    case 0:
+        InitializeEntity(g_EInitParticle);
+        self->drawFlags = ENTITY_ROTATE;
+        self->animSet = ANIMSET_OVL(1);
+        if (Random() & 1) {
+            self->animCurFrame = 0x26;
+        } else {
+            self->animCurFrame = 0x27;
+        }
+        if (self->velocityX < 0) {
+            self->facingLeft = 1;
+        }
+        // fallthrough
+    case 1:
+        MoveEntity();
+        self->rotate += 0x20;
+        if (self->params) {
+            self->rotate += 0x20;
+        }
+        self->velocityY += FIX(0.125);
+        posX = self->posX.i.hi;
+        posY = self->posY.i.hi + 6;
+        g_api.CheckCollision(posX, posY, &collider, 0);
+        if (collider.effects & EFFECT_SOLID) {
+            self->posY.i.hi += collider.unk18;
+            if (self->velocityY < FIX(0.5)) {
+                newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
+                if (newEntity != NULL) {
+                    CreateEntityFromEntity(
+                        E_INTENSE_EXPLOSION, self, newEntity);
+                    newEntity->params = 0x10;
+                }
+                DestroyEntity(self);
+                break;
+            }
+
+            self->velocityY = (-self->velocityY * 2) / 3;
         }
         break;
     }
