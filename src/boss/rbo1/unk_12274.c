a3f9de4cc32f501d0921ce217edeb01d5286bdde
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-07 23:39:55 -0300

    rbo1: decompile SpawnEntityCluster

--- src/boss/rbo1/unk_12274.c
+++ src/boss/rbo1/unk_12274.c
@@ -31,7 +31,27 @@ INCLUDE_ASM("boss/rbo1/nonmatchings/unk_12274", func_801CE228);
 
 INCLUDE_ASM("boss/rbo1/nonmatchings/unk_12274", polarPlacePartsList);
 
-INCLUDE_ASM("boss/rbo1/nonmatchings/unk_12274", func_us_8019D260_from_rcen);
+static void SpawnEntityCluster_from_rcen(void) {
+    s32 i;
+    u8 variant;
+    s16 angle;
+    Entity* newEntity;
+
+    variant = Random() & 0x3;
+    angle = ((Random() & 0xF) << 8) - 0x800;
+
+    for (i = 0; i < 6; i++) {
+        newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
+        if (newEntity != NULL) {
+            CreateEntityFromEntity(E_UNK_1D, g_CurrentEntity, newEntity);
+            newEntity->ext.et_8019D330.unk89 = 6 - i;
+            newEntity->ext.et_8019D330.unk88 = variant;
+            newEntity->params = 2;
+            newEntity->ext.et_8019D330.unk84 = angle;
+            newEntity->zPriority = g_CurrentEntity->zPriority + 1;
+        }
+    }
+}
 
 INCLUDE_ASM("boss/rbo1/nonmatchings/unk_12274", func_us_801947E4);
 
