96e78411752ef270dab22d3a0390bd25329b8f07
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-07 23:39:55 -0300

    rno2: decompile door proximity helpers and secret wall pair from bo0

--- src/st/rno2/unk_3459C.c
+++ src/st/rno2/unk_3459C.c
@@ -1,18 +1,145 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno2.h"
 
+extern EInit g_EInitEnvironment;
+
+void func_us_801AB9EC_from_bo0(Primitive* prim);
+
 INCLUDE_ASM("st/rno2/nonmatchings/unk_3459C", func_us_801AB9EC_from_bo0);
 
 INCLUDE_ASM("st/rno2/nonmatchings/unk_3459C", func_us_801B5FB8_from_no2);
 
-INCLUDE_ASM("st/rno2/nonmatchings/unk_3459C", func_us_801AC54C_from_bo0);
+void func_us_801AC54C_from_bo0(Entity* self) {
+    Primitive* prim;
+    s32 primIndex;
+    s32 i;
 
-INCLUDE_ASM("st/rno2/nonmatchings/unk_3459C", func_us_801AC73C_from_bo0);
+    switch (self->step) {
+    case 0:
+        InitializeEntity(g_EInitEnvironment);
+        self->hitboxState = 0;
+        self->animCurFrame = 0;
+        break;
+
+    case 1:
+        if (g_CastleFlags[RNO2_SECRET_WALL_OPEN] & 2) {
+            DestroyEntity(self);
+            return;
+        }
+        if (g_CastleFlags[RNO2_SECRET_WALL_OPEN]) {
+            g_CastleFlags[RNO2_SECRET_WALL_OPEN] |= 2;
+        }
+        primIndex = g_api.AllocPrimitives(PRIM_GT4, 8);
+        if (primIndex != -1) {
+            self->flags |= FLAG_HAS_PRIMS;
+            self->primIndex = primIndex;
+            prim = &g_PrimBuf[primIndex];
+            self->ext.breakableNo2.unk7C = prim;
+            while (prim != NULL) {
+                prim->drawMode = DRAW_HIDE;
+                prim = prim->next;
+            }
+        } else {
+            DestroyEntity(self);
+            return;
+        }
+        prim = self->ext.breakableNo2.unk7C;
+        for (i = 0; i < 4; i++) {
+            UnkPolyFunc2(prim);
+            prim->next->x1 = self->posX.i.hi;
+            prim->next->y0 = self->posY.i.hi;
+            prim->next->r3 = i + 8;
+            prim = prim->next;
+            prim = prim->next;
+        }
+        self->step++;
+        break;
+
+    case 2:
+        i = 1;
+        prim = self->ext.breakableNo2.unk7C;
+        while (prim != NULL) {
+            if (prim->p3 & 8) {
+                i = 0;
+                func_us_801AB9EC_from_bo0(prim);
+            }
+            prim = prim->next;
+        }
+        if (i != 0) {
+            DestroyEntity(self);
+            return;
+        }
+        break;
+    }
+}
+
+void func_us_801AC73C_from_bo0(Primitive* prim) {
+    s32 x, y;
+
+    if (!prim->g3) {
+        prim->u0 = 1;
+        prim->v0 = 1;
+        prim->r0 = 0x80;
+        prim->g0 = 0x80;
+        prim->b0 = 0xC0;
+        prim->drawMode = DRAW_UNK02;
+        prim->x0 = g_CurrentEntity->posX.i.hi;
+        prim->y0 = g_CurrentEntity->posY.i.hi + 8;
+        prim->x1 = 0;
+        prim->y1 = 0;
+        LOW(prim->x2) = 0x7000 - ((Random() & 7) << 0xD);
+        LOW(prim->x3) = 0x7000 - ((Random() & 7) << 0xD);
+        prim->g3 = 1;
+        prim->r3 = 0x20;
+    }
+#ifdef VERSION_US
+    x = (prim->x0 << 0x10) + (u16)prim->x1;
+#else
+    x = (prim->x0 << 0x10) + prim->x1;
+#endif
+    x += LOW(prim->x2);
+    prim->x0 = HIHU(x);
+    prim->x1 = LOHU(x);
+#ifdef VERSION_US
+    y = (prim->y0 << 0x10) + (u16)prim->y1;
+#else
+    y = (prim->y0 << 0x10) + prim->y1;
+#endif
+    y += LOW(prim->x3);
+    prim->y0 = HIH(y);
+    prim->y1 = LOH(y);
+    LOW(prim->x3) += 0x2000;
+    prim->r3 -= 1;
+    if (!prim->r3) {
+        prim->g3 = 0;
+        prim->drawMode = DRAW_HIDE;
+        prim->p3 = 0;
+    }
+}
 
 INCLUDE_ASM("st/rno2/nonmatchings/unk_3459C", func_us_801B68EC_from_no2);
 
 INCLUDE_ASM("st/rno2/nonmatchings/unk_3459C", EntityPrisoner);
 
-INCLUDE_ASM("st/rno2/nonmatchings/unk_3459C", func_us_801B5EE4);
+static bool SealedDoorIsNearPlayer(Entity* self) {
+    s16 distanceX;
+    s16 diffX;
+    s16 distanceY;
+    s16 diffY;
+
+    diffX = PLAYER.posX.i.hi - self->posX.i.hi;
+    distanceX = abs(diffX);
+    if (distanceX > 24) {
+        return false;
+    }
+
+    diffY = PLAYER.posY.i.hi - self->posY.i.hi;
+    distanceY = abs(diffY);
+    if (distanceY > 32) {
+        return false;
+    }
+
+    return true;
+}
 
 INCLUDE_ASM("st/rno2/nonmatchings/unk_3459C", EntitySealedDoor);
--- src/st/rno2/unk_39458.c
+++ src/st/rno2/unk_39458.c
@@ -1,6 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rno2.h"
 
-INCLUDE_ASM("st/rno2/nonmatchings/unk_39458", func_us_801B9458);
+static bool EntityIsNearPlayer(Entity* self) {
+    s16 distanceX;
+    s16 diffX;
+    s16 distanceY;
+    s16 diffY;
+
+    diffX = PLAYER.posX.i.hi - self->posX.i.hi;
+    distanceX = abs(diffX);
+    if (distanceX > 16) {
+        return false;
+    }
+
+    diffY = PLAYER.posY.i.hi - self->posY.i.hi;
+    distanceY = abs(diffY);
+    if (distanceY > 32) {
+        return false;
+    }
+
+    return true;
+}
 
 INCLUDE_ASM("st/rno2/nonmatchings/unk_39458", EntityRedDoor);
