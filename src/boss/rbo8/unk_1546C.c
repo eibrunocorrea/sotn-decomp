a5740ea706be920324927a024f3ba77d2a1dc5e0
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-08 03:09:30 -0300

    rbo8: decompile EntityBreakable and giantbro helpers

--- src/boss/rbo8/unk_1546C.c
+++ src/boss/rbo8/unk_1546C.c
@@ -1,14 +1,40 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rbo8.h"
 
-INCLUDE_ASM("boss/rbo8/nonmatchings/unk_1546C", EntityBreakable);
-
 INCLUDE_ASM("boss/rbo8/nonmatchings/unk_1546C", func_us_801955A0);
 
 INCLUDE_ASM("boss/rbo8/nonmatchings/unk_1546C", func_us_801955F8);
 
-INCLUDE_ASM("boss/rbo8/nonmatchings/unk_1546C", func_801CE1E8);
+void func_801CE1E8(s32 step) {
+    s32 i;
 
-INCLUDE_ASM("boss/rbo8/nonmatchings/unk_1546C", func_801CE228);
+    g_CurrentEntity->step = step;
+    g_CurrentEntity->step_s = 0;
+    g_CurrentEntity->pose = 0;
+    g_CurrentEntity->poseTimer = 0;
+    for (i = 0; i < 4; i++) {
+        g_CurrentEntity->ext.GH_Props.unkB0[i] = 0;
+        g_CurrentEntity->ext.GH_Props.unkB4[i] = 0;
+    }
+}
 
-INCLUDE_ASM("boss/rbo8/nonmatchings/unk_1546C", polarPlacePartsList);
+void func_801CE228() {
+    s32 i;
+
+    for (i = 0; i < 4; i++) {
+        g_CurrentEntity->ext.GH_Props.unkB0[i] = 0;
+        g_CurrentEntity->ext.GH_Props.unkB4[i] = 0;
+    }
+}
+
+void polarPlacePartsList(s16* offsets) {
+    Entity* entity;
+
+    while (*offsets) {
+        entity = g_CurrentEntity + *offsets;
+        if (!entity->ext.GH_Props.unkA8) {
+            polarPlacePart(entity);
+        }
+        offsets++;
+    }
+}
--- src/boss/rbo8/unk_15868.c
+++ src/boss/rbo8/unk_15868.c
@@ -1,7 +1,24 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rbo8.h"
 
-INCLUDE_ASM("boss/rbo8/nonmatchings/unk_15868", func_801CE3FC);
+void func_801CE3FC(s16* offsets) {
+    Entity* entity;
+    s32 i;
+
+    for (i = 0; i < 4; i++) {
+        entity = g_CurrentEntity + offsets[i];
+        polarPlacePart(entity);
+    }
+    offsets += 4;
+
+    while (*offsets) {
+        if (*offsets != 0xFF) {
+            entity = g_CurrentEntity + *offsets;
+            polarPlacePart(entity);
+        }
+        offsets++;
+    }
+}
 
 INCLUDE_ASM("boss/rbo8/nonmatchings/unk_15868", func_us_80195938);
 
