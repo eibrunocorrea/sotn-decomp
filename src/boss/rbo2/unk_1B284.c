f214d287e666233de6562294d3fbb6029f6b6359
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-07 23:39:55 -0300

    rbo2: decompile SpawnEntityCluster and share func_801CDC80

--- src/boss/rbo2/unk_1B284.c
+++ src/boss/rbo2/unk_1B284.c
@@ -5,7 +5,7 @@ INCLUDE_ASM("boss/rbo2/nonmatchings/unk_1B284", func_us_8019A98C_from_rcen);
 
 INCLUDE_ASM("boss/rbo2/nonmatchings/unk_1B284", func_us_8019B430);
 
-INCLUDE_ASM("boss/rbo2/nonmatchings/unk_1B284", func_801CDC80);
+#include "../../st/approach_s16.h"
 
 INCLUDE_ASM("boss/rbo2/nonmatchings/unk_1B284", func_us_8019B52C);
 
@@ -41,7 +41,27 @@ INCLUDE_ASM("boss/rbo2/nonmatchings/unk_1B284", func_801CE228);
 
 INCLUDE_ASM("boss/rbo2/nonmatchings/unk_1B284", polarPlacePartsList);
 
-INCLUDE_ASM("boss/rbo2/nonmatchings/unk_1B284", func_us_8019D260_from_rcen);
+static void SpawnEntityCluster_from_rcen(void) {
+    s32 i;
+    u8 variant;
+    s16 angle;
+    Entity* newEntity;
+
+    variant = Random() & 0x3;
+    angle = ((Random() & 0xF) << 8) - 0x800;
+
+    for (i = 0; i < 6; i++) {
+        newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
+        if (newEntity != NULL) {
+            CreateEntityFromEntity(E_UNK_1F, g_CurrentEntity, newEntity);
+            newEntity->ext.et_8019D330.unk89 = 6 - i;
+            newEntity->ext.et_8019D330.unk88 = variant;
+            newEntity->params = 2;
+            newEntity->ext.et_8019D330.unk84 = angle;
+            newEntity->zPriority = g_CurrentEntity->zPriority + 1;
+        }
+    }
+}
 
 INCLUDE_ASM("boss/rbo2/nonmatchings/unk_1B284", func_us_8019F260);
 
