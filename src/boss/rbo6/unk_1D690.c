c1920085f740a7a83203cd05e67324296329da8c
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-07 23:39:55 -0300

    rbo6: decompile SpawnEntityCluster

--- src/boss/rbo6/unk_1D690.c
+++ src/boss/rbo6/unk_1D690.c
@@ -1,7 +1,27 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "rbo6.h"
 
-INCLUDE_ASM("boss/rbo6/nonmatchings/unk_1D690", func_us_8019D260_from_rcen);
+static void SpawnEntityCluster_from_rcen(void) {
+    s32 i;
+    u8 variant;
+    s16 angle;
+    Entity* newEntity;
+
+    variant = Random() & 0x3;
+    angle = ((Random() & 0xF) << 8) - 0x800;
+
+    for (i = 0; i < 6; i++) {
+        newEntity = AllocEntity(&g_Entities[224], &g_Entities[256]);
+        if (newEntity != NULL) {
+            CreateEntityFromEntity(E_UNK_16, g_CurrentEntity, newEntity);
+            newEntity->ext.et_8019D330.unk89 = 6 - i;
+            newEntity->ext.et_8019D330.unk88 = variant;
+            newEntity->params = 2;
+            newEntity->ext.et_8019D330.unk84 = angle;
+            newEntity->zPriority = g_CurrentEntity->zPriority + 1;
+        }
+    }
+}
 
 INCLUDE_ASM("boss/rbo6/nonmatchings/unk_1D690", func_us_8019D330_from_rcen);
 
