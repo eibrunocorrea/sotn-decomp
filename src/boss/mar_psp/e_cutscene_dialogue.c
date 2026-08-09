ae6174e826944e35ea961ddd46ccc1dc9f76b7f7
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-09 04:29:13 -0300

    mar: wire psp e_cutscene_dialogue to shared cutscene helpers

--- src/boss/mar_psp/e_cutscene_dialogue.c
+++ src/boss/mar_psp/e_cutscene_dialogue.c
@@ -1,14 +1,22 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../mar/mar.h"
+#include <cutscene.h>
 
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/e_cutscene_dialogue", GetLang);
+// helpers are called from raw-asm functions in sibling objects
+#define CUTSCENE_GLOBAL
 
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/e_cutscene_dialogue", DrawCutsceneActorName);
+extern Dialogue g_Dialogue;
 
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/e_cutscene_dialogue", CutsceneUnk3);
+#include "../../get_lang.h"
 
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/e_cutscene_dialogue", CutsceneUnk4);
+#include "../../st/cutscene_actor_name.h"
 
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/e_cutscene_dialogue", CutsceneUnk1);
+#include "../../st/cutscene_unk3.h"
 
-INCLUDE_ASM("boss/mar_psp/nonmatchings/mar_psp/e_cutscene_dialogue", SetCutsceneScript);
+#include "../../st/cutscene_unk4.h"
+
+#define CUTSCENE_UNK1_NEXT_X 0
+#define CUTSCENE_UNK1_UNK17 2
+#include "../../st/cutscene_unk1.h"
+
+#include "../../st/set_cutscene_script.h"
