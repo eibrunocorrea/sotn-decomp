40ce503a4c5c3b29741fe5829dafde1573d506b6
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-09 04:17:48 -0300

    bo5: wire psp e_cutscene_dialogue to shared cutscene helpers

--- src/boss/bo5/e_cutscene_dialogue.c
+++ src/boss/bo5/e_cutscene_dialogue.c
@@ -5,9 +5,29 @@
 // helpers are called from raw-asm functions in sibling objects
 #define CUTSCENE_GLOBAL
 
+// e_cutscene/cutscene_script_text
+#ifdef VERSION_PSP
+#define CUTSCENE_UNK1_NEXT_X 0
+#define CUTSCENE_UNK1_UNK17 2
+#endif
+
 extern Dialogue g_Dialogue;
 extern const char* actor_names[];
 
+#ifdef VERSION_PSP
+// psp emits the helpers in a different order than psx
+#include "../../get_lang.h"
+
+#include "../../st/cutscene_actor_name.h"
+
+#include "../../st/cutscene_unk3.h"
+
+#include "../../st/cutscene_unk4.h"
+
+#include "../../st/cutscene_unk1.h"
+
+#include "../../st/set_cutscene_script.h"
+#else
 #include "../../st/cutscene_unk1.h"
 
 #include "../../st/set_cutscene_script.h"
@@ -17,3 +37,4 @@ extern const char* actor_names[];
 #include "../../st/cutscene_unk4.h"
 
 #include "../../st/cutscene_actor_name.h"
+#endif
