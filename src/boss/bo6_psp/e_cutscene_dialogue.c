de3a7d5b644b15f7b2214fea98ddf389b079b2f7
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-09 04:29:13 -0300

    bo6: wire psp e_cutscene_dialogue to shared cutscene helpers

--- src/boss/bo6_psp/e_cutscene_dialogue.c
+++ src/boss/bo6_psp/e_cutscene_dialogue.c
@@ -1,14 +1,25 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../bo6/bo6.h"
+#include <cutscene.h>
 
-INCLUDE_ASM("boss/bo6_psp/nonmatchings/bo6_psp/e_cutscene_dialogue", GetLang);
+// helpers are called from raw-asm functions in sibling objects
+#define CUTSCENE_GLOBAL
 
-INCLUDE_ASM("boss/bo6_psp/nonmatchings/bo6_psp/e_cutscene_dialogue", DrawCutsceneActorName);
+extern Dialogue g_Dialogue;
 
-INCLUDE_ASM("boss/bo6_psp/nonmatchings/bo6_psp/e_cutscene_dialogue", CutsceneUnk3);
+#include "../../get_lang.h"
 
-INCLUDE_ASM("boss/bo6_psp/nonmatchings/bo6_psp/e_cutscene_dialogue", CutsceneUnk4);
+#include "../../st/cutscene_actor_name.h"
 
-INCLUDE_ASM("boss/bo6_psp/nonmatchings/bo6_psp/e_cutscene_dialogue", CutsceneUnk1);
+// bo6 draws the dialogue on the right half of the framebuffer
+#define CUTSCENE_UNK3_RECT_X 256
+#include "../../st/cutscene_unk3.h"
 
-INCLUDE_ASM("boss/bo6_psp/nonmatchings/bo6_psp/e_cutscene_dialogue", SetCutsceneScript);
+#define CUTSCENE_UNK4_TPAGE 0x14
+#include "../../st/cutscene_unk4.h"
+
+#define CUTSCENE_UNK1_NEXT_X 256
+#define CUTSCENE_UNK1_UNK17 2
+#include "../../st/cutscene_unk1.h"
+
+#include "../../st/set_cutscene_script.h"
