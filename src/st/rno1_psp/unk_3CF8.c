29a10f381f4fe30bf66323e3166cb122afccb1d9
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-09 03:18:35 -0300

    rno1: wire psp e_particles

--- src/st/rno1_psp/unk_3CF8.c
+++ src/st/rno1_psp/unk_3CF8.c
@@ -8,7 +8,3 @@ INCLUDE_ASM("st/rno1_psp/nonmatchings/rno1_psp/unk_3CF8", EntityJackOBones);
 INCLUDE_ASM("st/rno1_psp/nonmatchings/rno1_psp/unk_3CF8", EntityJackOBonesDeathParts);
 
 INCLUDE_ASM("st/rno1_psp/nonmatchings/rno1_psp/unk_3CF8", EntityJackOBonesJack);
-
-INCLUDE_ASM("st/rno1_psp/nonmatchings/rno1_psp/unk_3CF8", func_psp_0923AD68);
-
-INCLUDE_ASM("st/rno1_psp/nonmatchings/rno1_psp/unk_3CF8", func_psp_0923B2F0);
