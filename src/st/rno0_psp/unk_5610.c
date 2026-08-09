e7ae36af5a42a48c13feacb14a2b27728aad8b2a
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-09 05:11:31 -0300

    rno0: wire psp e_jack_o_bones quartet (anchor split from unk_5610)

--- src/st/rno0_psp/unk_5610.c
+++ src/st/rno0_psp/unk_5610.c
@@ -1,14 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 #include "../rno0/rno0.h"
 
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_5610", TryThrow);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_5610", EntityJackOBones);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_5610", EntityJackOBonesDeathParts);
-
-INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_5610", EntityJackOBonesJack);
-
 INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_5610", func_pspeu_0923D928);
 
 INCLUDE_ASM("st/rno0_psp/nonmatchings/rno0_psp/unk_5610", func_us_801D2424);
