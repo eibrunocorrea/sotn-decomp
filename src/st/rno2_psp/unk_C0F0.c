4e976b384a089317b46ec715264f278adaae00e4
Bruno Correa <179166498+eibrunocorrea@users.noreply.github.com>
2026-08-09 03:18:35 -0300

    rno2: wire psp e_particles

--- src/st/rno2_psp/unk_C0F0.c
+++ src/st/rno2_psp/unk_C0F0.c
@@ -12,7 +12,3 @@ INCLUDE_ASM("st/rno2_psp/nonmatchings/rno2_psp/unk_C0F0", func_us_801C4EA8);
 INCLUDE_ASM("st/rno2_psp/nonmatchings/rno2_psp/unk_C0F0", EntityFlyingZombie2);
 
 INCLUDE_ASM("st/rno2_psp/nonmatchings/rno2_psp/unk_C0F0", EntityFlyingZombie1);
-
-INCLUDE_ASM("st/rno2_psp/nonmatchings/rno2_psp/unk_C0F0", func_psp_0923AD68);
-
-INCLUDE_ASM("st/rno2_psp/nonmatchings/rno2_psp/unk_C0F0", func_psp_0923B2F0);
